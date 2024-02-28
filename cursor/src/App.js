78f951747320b89652deb21fcf39db064131cfa6
spoorthi <spoorthiachar9@gmail.com>
2024-02-28 13:17:43 +0530

    first cursor commit

---
+++ cursor/src/App.js
@@ -0,0 +1,17 @@
+import './index.css';
+import React from 'react';
+import ParticlesBackground from './components/config/particleBackground';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <ParticlesBackground />
+      <div className='particlesheader'>
+        prticle.js
+        
+      </div>
+    </div>
+  );
+};
+
+export default App;
